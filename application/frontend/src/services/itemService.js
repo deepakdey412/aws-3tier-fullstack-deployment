f05f161e06f52f03259a7216a8d679c128de2164
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-06-29 22:33:24 +0530

    updates frontend issue

--- application/frontend/src/services/itemService.js
+++ application/frontend/src/services/itemService.js
@@ -6,31 +6,31 @@ const itemService = {
   // Get all items
   async getAllItems() {
     const response = await axios.get(API_URL)
-    return response.data
+    return response.data.data
   },
 
   // Get item by ID
   async getItemById(id) {
     const response = await axios.get(`${API_URL}/${id}`)
-    return response.data
+    return response.data.data
   },
 
   // Create new item
   async createItem(itemData) {
     const response = await axios.post(API_URL, itemData)
-    return response.data
+    return response.data.data
   },
 
   // Update item
   async updateItem(id, itemData) {
     const response = await axios.put(`${API_URL}/${id}`, itemData)
-    return response.data
+    return response.data.data
   },
 
   // Delete item
   async deleteItem(id) {
     const response = await axios.delete(`${API_URL}/${id}`)
-    return response.data
+    return response.data.data
   },
 
   // Search items
@@ -38,7 +38,7 @@ const itemService = {
     const response = await axios.get(`${API_URL}/search`, {
       params: { q: query }
     })
-    return response.data
+    return response.data.data
   }
 }
 
